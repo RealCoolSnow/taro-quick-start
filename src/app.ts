e03f1b8b09c2c169638799f435583f377c6895b2
CoolSnow <coolsnow2020@gmail.com>
2021-02-04 11:37:18 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import './app.less'
 import 'vant/lib/index.css'
 import store from './store'
 const app = createApp({
-  onShow(options) {
+  setup(options) {
     console.log('app created')
   },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
